f7c98ca514e27f531963ff81f1bc9281ce52ea40
LucasLem0s <lucaslemoss@live.com>
2025-09-12 10:01:00 -0300

    Criando lista e funções

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value; 
+
+    if (nome === "") {
+        alert("Por favor, insira um nome!");
+        return;
+    }
+
+    let apenasNomes = /^[A-Za-záéíóúãõâêîôûàèìòù\s]+$/;
+
+    if (!apenasNomes.test(nome)) {
+        alert("Por favor, insira um nome válido (apenas letras)!");
+        return; 
+    }
+}
